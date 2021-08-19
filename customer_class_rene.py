82feb0f63cd3cafccc2fa8d0e2e7fb97e7803983
René Kuhn <renekuhnpower@gmail.com>
2021-08-19 16:17:58 +0200

    implemented random weigthed choice in customer_class_rene.py Customer.next_state() function

--- customer_class_rene.py
+++ customer_class_rene.py
@@ -1,4 +1,5 @@
 import random
+import pandas as pd
 
 
 class Customer:
@@ -7,10 +8,18 @@ class Customer:
     in a MCMC simulation
     """
 
-    def __init__(self,name,state,budget=100):
+    def is_active(self):
+        """Returns False if a customer has reached the checkout,other wise returns True """
+        if self.state == 'checkout':
+            return False
+        else:
+            return True
+
+    def __init__(self, name, state, transition_probs=0, budget=100):
         self.name = name
         self.state = state
-        self.budget= budget
+        self.budget = budget
+        self.transition_probs = pd.read_csv('trans_prob.csv',index_col=0)
 
     def __repr__(self):
         return f'<Customer {self.name} in {self.state}>'
@@ -20,17 +29,19 @@ class Customer:
         propagates the customer to the next state , returns nothing
         :return: nothing
         """
-        self.state = random.choice(['spices','drinks','fruit'])
+        print(self.transition_probs.index)
+        print('---------')
+        print(self.transition_probs[self.state])
+        self.state = random.choices(list(self.transition_probs.index), weights=self.transition_probs[self.state])[0]
 
-cust1 = Customer('Jake','drinks',50)
-cust2 = Customer('Margret','spices')
 
 
+cust1 = Customer('Jake', 'drinks', 50)
+cust2 = Customer('Margret', 'spices')
 
-print(cust1.name,cust1.state)
-print(cust2.name,cust2.budget)
-
+print(cust1.name, cust1.state)
+print(cust2.name, cust2.budget)
 
 cust1.next_state()
 print('--------------------')
-print(cust1.name,cust1.state)
+print(cust1.name, cust1.state)
